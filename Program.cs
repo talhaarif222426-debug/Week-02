780eb80f17c11779584a80372fab6ab6ceb08dc2
talhaarif222426-debug <talhaarif222426@gmail.com>
2026-03-08 21:54:18 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transaction t1 = new Transaction(1, "Mobile", 70000, "03-07-2026");
+
+            // copying existing object into new object.
+            Transaction t2 = new Transaction(t1);
+
+            // these are some new values to check wherther changing values effect the existing value or not.
+            //t2.Transaction_Id = 2;
+            //t2.ProductName = "Laptop";
+            //t2.Amount = 120000;
+            //t2.DateTime = "12-8-2025";
+
+            Console.WriteLine("Original Transaction:");
+            t1.Display();
+
+            Console.WriteLine("\nCopied Transaction:");
+            t2.Display();
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Week_2_Task_01
+{
+    internal class Transaction
+    {
+        public int Transaction_Id;
+        public string ProductName;
+        public double Amount;
+        public string DateTime;
+
+        // Parametrixed Constructor to store values.
+        public Transaction(int Id, string Name, double amount, string dateTime)
+        {
+            Transaction_Id = Id;
+            ProductName = Name;
+            Amount = amount;
+            DateTime = dateTime;
+        }
+
+        // Copy constructor.
+        public Transaction(Transaction t1)
+        {
+            Transaction_Id = t1.Transaction_Id;
+            ProductName = t1.ProductName;
+            Amount = t1.Amount;
+            DateTime = t1.DateTime;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Transaction ID: " + Transaction_Id);
+            Console.WriteLine("Product Name: " + ProductName );
+            Console.WriteLine("Amount : " + Amount);
+            Console.WriteLine("Date Time: " + DateTime);
+        }
+    }
+}
